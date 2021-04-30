48e506f2a821d4658b9cdf1375213c91bb1f9842
ethuringer <ethuringer@gmail.com>
2021-04-30 11:30:56 +0200

    js-task ready

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,23 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+    const navLinks = document.querySelectorAll('nav#link-list a');
+    navLinks.forEach( item => {
+        if ( item.innerHTML.includes('outer-link') ) {
+            item.target ='_blank';
+            item.innerHTML = `<strong>${item.innerHTML}</strong>`;
+        }
+    })
 
+    const navElements = document.querySelectorAll('nav');
+    navElements.forEach(el => {
+        el.style.display = 'flex';
+        el.style.flexDirection = 'column';
+        el.style.margin = '0px auto';
+        el.style.width = '30%';
+        el.style.border = '1px solid blue';
+        el.style.padding = '16px';
+      });
+}
+
+changeOuterLinks();
 
 export { changeOuterLinks };
